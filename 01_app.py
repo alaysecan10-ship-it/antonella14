2d76dc788d4a7a1a978d480fe09b9effdc05a4e5
alaysecan10-ship-it <alaysecan10@gmail.com>
2025-09-30 02:31:14 +0000

    primer ejemplo con bucle

---
+++ 01_app.py
@@ -0,0 +1,14 @@
+# Estructura de bucle for
+
+# Sintaxis
+# For value in sequence:
+# instruccion que es lo que debe ahcer mientras se cumple el bucle
+
+# Ejemplo:
+
+# Listado de frutas
+frutas = ["manzana" , "mandarina" , "arándanos" , "fresas" , "maracuyá"]
+
+for fruta in frutas:
+    print(fruta)
+    
