6da3b0fe4d302f6954092af5de63ea3097e13b0a
alaysecan10-ship-it <alaysecan10@gmail.com>
2025-09-30 03:26:02 +0000

    pregunta 2 examen t1

---
+++ 03_app.py
@@ -0,0 +1,18 @@
+# Estructura condicional If (si) - elif - else (sino)
+
+# Sintaxis
+# if condición:
+#   instrucciones a ejecutarse si la condición es verdadera 
+# elif condición:
+#   instrucciones a ejecutar para la segunda condición
+# else:
+#   instrucciones a ejecutar si la condición es falsa
+
+
+
+
+
+
+
+
+
